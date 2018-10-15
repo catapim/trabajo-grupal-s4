b923a3c71a881bd5a1013ea4f41690e879d46602
toxocarrier <catalinapimentel@gmail.com>
2018-10-15 03:28:18 -0300

    haciendo algunos fixes
    aun no funciona!

--- base/app/src/main/java/e/francisco/base/MainActivity.java
+++ base/app/src/main/java/e/francisco/base/MainActivity.java
@@ -1,20 +1,43 @@
 package e.francisco.base;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 import e.francisco.base.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btn_guardar_libro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
+        setContentView(R.layout.registrolibro);
+
+        btn_guardar_libro = (Button) findViewById(R.id.btn_guardar_libro);
+
+        @Override
+        public void onClick(View v) {
+            txtContent.setText(FileHelper.ReadFile(MainActivity.this));
+        }
+        
+
+
+
+
+
+        //if (savedInstanceState == null) {
+        //    getSupportFragmentManager().beginTransaction()
+        //           .replace(R.id.container, MainFragment.newInstance())
+        //           .commitNow();
         }
     }
+
 }
