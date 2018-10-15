62e6591e0893e439b76de0828422708b6653bd51
toxocarrier <catalinapimentel@gmail.com>
2018-10-15 10:35:57 -0300

    borre cosas q no servian porque al final sera array y no txt

--- base/app/src/main/java/e/francisco/base/MainActivity.java
+++ base/app/src/main/java/e/francisco/base/MainActivity.java
@@ -3,11 +3,13 @@ package e.francisco.base;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
+import java.util.ArrayList;
 
 import e.francisco.base.ui.main.MainFragment;
 
@@ -15,22 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     Button btn_guardar_libro;
+    private ArrayList<String> libros = new ArrayList<String>();
+    private EditText intitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrolibro);
 
+        libros = new ArrayList<String>();
         btn_guardar_libro = (Button) findViewById(R.id.btn_guardar_libro);
 
-        @Override
-        public void onClick(View v) {
-            txtContent.setText(FileHelper.ReadFile(MainActivity.this));
-        }
-        
-
-
-
+        intitulo = (EditText) findViewById(R.id.input_titulo);
+        String ag=intitulo.getText().toString();
+        libros.add(ag);
+        intitulo.setText("");
 
 
         //if (savedInstanceState == null) {
@@ -39,5 +40,3 @@ public class MainActivity extends AppCompatActivity {
         //           .commitNow();
         }
     }
-
-}
