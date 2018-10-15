c48f0c41fb26294090460cb0cdfdcf1720cdc695
toxocarrier <catalinapimentel@gmail.com>
2018-10-15 11:48:10 -0300

    get input

--- base/app/src/main/java/e/francisco/base/MainActivity.java
+++ base/app/src/main/java/e/francisco/base/MainActivity.java
@@ -3,6 +3,7 @@ package e.francisco.base;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,24 +12,37 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText input_titulo = (EditText) findViewById(R.id.input_titulo);
-    EditText in_autor = (EditText) findViewById(R.id.input_autor);
-    EditText in_publicacion = (EditText) findViewById(R.id.input_publicacion);
-    EditText in_calificacion = (EditText) findViewById(R.id.input_calificacion);
-    //btn_guardar_libro = (Button) findViewByID(R.id.btn_guardar_libro);
-   // btn_guardar_libro.setOnClickListener(new View.OnclickListener(){
+
+    String titulo, autor, publicacion, calificacion;
+    EditText input_titulo;
+    Button btn_guardar_libro;
+    private ArrayList<String> libros = new ArrayList<String>();
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrolibro);
+        final EditText input_titulo = (EditText) findViewById(R.id.input_titulo);
+        //EditText in_autor = (EditText) findViewById(R.id.input_autor);
+        //EditText in_publicacion = (EditText) findViewById(R.id.input_publicacion);
+        //EditText in_calificacion = (EditText) findViewById(R.id.input_calificacion);
+        libros = new ArrayList<String>();
+        System.out.println("holap");
+        btn_guardar_libro = (Button) findViewById(R.id.btn_guardar_libro);
+        btn_guardar_libro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                titulo = input_titulo.getText().toString();
+            }
+        });
+        }
+
 
       //  }
   //  )
-    private ArrayList<String> libros = new ArrayList<String>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrolibro);
-        libros = new ArrayList<String>();
-        System.out.println("holap");
 
-    }
+
 }
 
