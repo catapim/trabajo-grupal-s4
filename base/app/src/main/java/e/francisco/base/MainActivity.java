e2d3c1408feeb7192bccfbc3fc885c61a97e8dd1
toxocarrier <catalinapimentel@gmail.com>
2018-10-15 11:38:35 -0300

    update main

--- base/app/src/main/java/e/francisco/base/MainActivity.java
+++ base/app/src/main/java/e/francisco/base/MainActivity.java
@@ -11,11 +11,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText in_titulo = (EditText) findViewById(R.id.input_titulo);
+    EditText input_titulo = (EditText) findViewById(R.id.input_titulo);
     EditText in_autor = (EditText) findViewById(R.id.input_autor);
     EditText in_publicacion = (EditText) findViewById(R.id.input_publicacion);
     EditText in_calificacion = (EditText) findViewById(R.id.input_calificacion);
-    Button btn_guardar_libro;
+    //btn_guardar_libro = (Button) findViewByID(R.id.btn_guardar_libro);
+   // btn_guardar_libro.setOnClickListener(new View.OnclickListener(){
+
+      //  }
+  //  )
     private ArrayList<String> libros = new ArrayList<String>();
 
     @Override
@@ -23,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrolibro);
         libros = new ArrayList<String>();
-        btn_guardar_libro = (Button) findViewById(R.id.btn_guardar_libro);
+        System.out.println("holap");
+
     }
 }
 
