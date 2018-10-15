d37a6b0280a5ef018112abb3bc44328cd906ea2e
toxocarrier <catalinapimentel@gmail.com>
2018-10-15 11:26:46 -0300

    declarando variables

--- base/app/src/main/java/e/francisco/base/MainActivity.java
+++ base/app/src/main/java/e/francisco/base/MainActivity.java
@@ -3,40 +3,27 @@ package e.francisco.base;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-
 import java.util.ArrayList;
 
-import e.francisco.base.ui.main.MainFragment;
-
 public class MainActivity extends AppCompatActivity {
-
-
+    EditText in_titulo = (EditText) findViewById(R.id.input_titulo);
+    EditText in_autor = (EditText) findViewById(R.id.input_autor);
+    EditText in_publicacion = (EditText) findViewById(R.id.input_publicacion);
+    EditText in_calificacion = (EditText) findViewById(R.id.input_calificacion);
     Button btn_guardar_libro;
     private ArrayList<String> libros = new ArrayList<String>();
-    private EditText intitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrolibro);
-
         libros = new ArrayList<String>();
         btn_guardar_libro = (Button) findViewById(R.id.btn_guardar_libro);
-
-        intitulo = (EditText) findViewById(R.id.input_titulo);
-        String ag=intitulo.getText().toString();
-        libros.add(ag);
-        intitulo.setText("");
-
-
-        //if (savedInstanceState == null) {
-        //    getSupportFragmentManager().beginTransaction()
-        //           .replace(R.id.container, MainFragment.newInstance())
-        //           .commitNow();
-        }
     }
+}
+
